05dbb1a7280a0793f762850e6d91ab53a4b7e2d3
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-11 18:13:40 -0300

    añadimos la funcion de crear usuario por el administrador y tambien añadimos el listar usuarios

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -3,10 +3,10 @@ import bcrypt from "bcrypt";
 import generarJWT from "../helper/generarJWT.js";
 
 const isActive = (body, boolean) => {
-    const usuario = body;
-    usuario.isActive = boolean;
-    return usuario;
-}
+  const usuario = body;
+  usuario.isActive = boolean;
+  return usuario;
+};
 export const crearUsuario = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -18,7 +18,49 @@ export const crearUsuario = async (req, res) => {
     }
     const saltos = bcrypt.genSaltSync(12);
     const passEncriptada = bcrypt.hashSync(password, saltos);
-    const usuario = isActive(req.body, true)
+    const usuario = isActive(req.body, true);
+    const nuevoUsuario = new Usuario(usuario);
+    nuevoUsuario.password = passEncriptada;
+    nuevoUsuario.save();
+    const token = await generarJWT(nuevoUsuario._id, nuevoUsuario.email);
+    res.status(201).json({
+      mensaje: "Usuario creado correctamente.",
+      email: nuevoUsuario.email,
+      nombreUsuario: nuevoUsuario.nombreUsuario,
+      token: token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      mensaje: "Error al intentar crear un usuario.",
+    });
+  }
+};
+
+export const listarUsuarios = async (req, res) => {
+  try {
+    const usuarios = await Usuario.find().select('email nombreUsuario perfilRGB isActive');
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(404)
+      .json({ mensaje: "No se pudo encontrar la lista de usuarios" });
+  }
+};
+
+export const crearUsuarioAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const emailValidacion = await Usuario.findOne({ email });
+    if (emailValidacion) {
+      return res.status(400).json({
+        mensaje: "Este correo ya se encuentra registrado.",
+      });
+    }
+    const saltos = bcrypt.genSaltSync(12);
+    const passEncriptada = bcrypt.hashSync(password, saltos);
+    const usuario = isActive(req.body, false);
     const nuevoUsuario = new Usuario(usuario);
     nuevoUsuario.password = passEncriptada;
     nuevoUsuario.save();
@@ -58,6 +100,7 @@ export const login = async (req, res) => {
     const token = await generarJWT(usuarioBuscado._id, usuarioBuscado.email);
     const usuario = isActive(usuarioBuscado, true);
     const nuevoUsuario = new Usuario(usuario);
+    nuevoUsuario.save();
     res.status(200).json({
       mensaje: "Los datos del usuario son correctos",
       email: email,
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { crearUsuario, login } from "../controllers/usuarios.controller.js";
+import { crearUsuario, crearUsuarioAdmin, listarUsuarios, login } from "../controllers/usuarios.controller.js";
 
 const router = Router();
 
 router.route("/registrar").post(crearUsuario)
 router.route("/login").post(login)
+router.route("/registroAdmin").post(crearUsuarioAdmin)
+router.route("/usuarios").get(listarUsuarios)
 
 export default router
