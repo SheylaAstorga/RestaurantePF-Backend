87a7d2523fb9ace18e775f1569b421168fdf98cb
Javier Jimenez <javier.sniguel@gmail.com>
2024-04-12 18:48:00 -0300

    se cambio los controllers aplicando las funciones de tokens en Helpes

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -1,6 +1,4 @@
-import { Usuario } from '../database/model/usuarios.js';
 import Pedido from '../database/model/pedido.js';
-import jwt from 'jsonwebtoken';
 
 export const listarPedidos = async (req, res) => {
   try {
@@ -24,52 +22,22 @@ export const obtenerPedido = async (req, res) => {
   }
 };
 
-// Middleware de autenticación
-const authMiddleware = async (req, res, next) => {
+export const crearPedido = async (req, res) => {
   try {
-    // Obtener el token de autenticación de los encabezados de la solicitud
-    const token = req.headers.authorization.split(' ')[1];
-
-    // Verificar y decodificar el token
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-
-    // Buscar el usuario en la base de datos
-    const usuario = await Usuario.findById(decoded.userId);
-
-    // Agregar el usuario al objeto de la solicitud
-    req.user = usuario;
-
-    next();
+    const pedidoNuevo = new Pedido({
+      ...req.body,
+      usuario: req.user._id,
+    });
+    await pedidoNuevo.save();
+    res.status(201).json({ mensaje: 'Pedido guardado' });
   } catch (error) {
-    res.status(401).json({ mensaje: 'Token de autenticación inválido' });
+    console.log(error);
+    res.status(400).json({ mensaje: 'No se pudo procesar la solicitud de crear pedido' });
   }
 };
 
-// Ruta para crear un pedido
-export const crearPedido = [
-  authMiddleware,
-  async (req, res) => {
-    try {
-      // Verificar si el usuario está autenticado
-      if (!req.user) {
-        return res.status(401).json({ mensaje: 'Usuario no autenticado' });
-      }
-
-      const pedidoNuevo = new Pedido({
-        ...req.body,
-        usuario: req.user._id,
-      });
-      await pedidoNuevo.save();
-      res.status(201).json({ mensaje: 'Pedido guardado' });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ mensaje: 'No se pudo procesar la solicitud de crear pedido' });
-    }
-  },
-];
-
-export const ModPedido = async(req,res) =>{
-  try{
+export const ModPedido = async (req, res) => {
+  try {
     const buscarPedido = await Pedido.findById(req.params.id);
     if (!buscarPedido) {
       return res
@@ -80,8 +48,8 @@ export const ModPedido = async(req,res) =>{
     }
     await Pedido.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json({ mensaje: "el pedido se modifico exitosamente" });
-  }catch(err){
+  } catch (err) {
     console.error(err);
-    res.status(400).json({mensaje:'no se pudo modificar el pedido'})
+    res.status(400).json({ mensaje: 'no se pudo modificar el pedido' });
   }
-}
+};
--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { ModPedido, crearPedido, listarPedidos, obtenerPedido } from '../controllers/pedidos.controllers.js';
+import validarJWT from '../helper/verificarJWT.js';
+
 
 const router = Router();
 
-router.route('/pedidos').get(listarPedidos).post(crearPedido);
+router.route('/pedidos').get(listarPedidos).post([validarJWT],crearPedido);
 router.route('/pedidos/:id').get(obtenerPedido).put(ModPedido);
 
 export default router;
