0f79e968aa0b4b39198dfead44a13f826cb05791
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-13 16:56:41 -0300

    añadimos la validacion de registrar admin

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
 import { borrarUsuario, crearUsuario, crearUsuarioAdmin, habilitarUsuario, listarUsuarios, login, suspenderUsuario } from "../controllers/usuarios.controller.js";
+import { validacionRegistroAdmin } from "../helper/validarUsuarios.js";
 
 const router = Router();
 
 router.route("/registrar").post(crearUsuario);
 router.route("/login").post(login);
-router.route("/registroAdmin").post(crearUsuarioAdmin);
+router.route("/registroAdmin").post([validacionRegistroAdmin], crearUsuarioAdmin);
 router.route("/usuarios").get(listarUsuarios);
 router.route("/suspenderUsuario").post(suspenderUsuario);
 router.route("/borrarUsuario").post(borrarUsuario);
