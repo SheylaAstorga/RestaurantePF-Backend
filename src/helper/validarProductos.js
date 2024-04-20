518e3ec743c59f2fd659163be255dc2a1a25b754
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-20 18:05:24 -0300

    añadimos la hoja de validacion

---
+++ src/helper/validarProductos.js
@@ -0,0 +1,8 @@
+import { check } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion.js";
+
+const validacionProductos =() => {
+
+}
+
+export default validacionProductos
