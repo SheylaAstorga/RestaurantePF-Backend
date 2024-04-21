d2ea8b2e671ad55e6836970f236aa1f35fbc568c
Fiol Thiago <fiolthiago9@gmail.com>
2024-04-21 19:38:57 -0300

    añadimos el verficar el nombre de usuario y tambien el validar productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {borrarProducto, crearProducto, listarProductos, modificarProducto, obtenerProducto} from "../controllers/productos.controllers.js"
+import validacionProductos from "../helper/validarProductos.js";
 
 
 const router = Router();
 
-router.route('/productos').get(listarProductos).post(crearProducto)
-router.route('/producto/:id').get(obtenerProducto).delete(borrarProducto).put(modificarProducto)
+router.route('/productos').get(listarProductos).post([validacionProductos],crearProducto)
+router.route('/producto/:id').get(obtenerProducto).delete(borrarProducto).put([validacionProductos],modificarProducto)
 
 export default router;
